b7a68a488c1d405edea272e183a64a80a54ecd12
PENG TANG <pengtang@PENGs-MacBook-Pro.local>
2020-09-30 13:22:00 +0800

    finished CAT auto-configuration

---
+++ leqee-boot-autoconfiguration/src/main/java/com/leqee/boot/autoconfiguration/cat/LeqeeBootCatAutoConfiguration.java
@@ -0,0 +1,110 @@
+package com.leqee.boot.autoconfiguration.cat;
+
+import com.dianping.cat.Cat;
+import com.dianping.cat.configuration.client.entity.ClientConfigProperty;
+import com.dianping.cat.log.CatLogger;
+import com.dianping.cat.servlet.CatFilter;
+import net.dubboclub.catmonitor.DubboCat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.io.File;
+
+@Configuration
+@ConditionalOnClass(name = {"com.leqee.boot.autoconfiguration.annotation.EnableCat"})
+@PropertySource("classpath:leqee-cat.properties")
+@ConfigurationProperties(prefix = "leqee-boot.cat")
+public class LeqeeBootCatAutoConfiguration {
+
+    private int port;
+
+    private int httpPort;
+
+    private String[] servers;
+
+    @Value("${spring.application.name:unknown}")
+    private String applicationName;
+
+    @Autowired
+    private LeqeeBootCatAutoConfiguration catAutoConfiguration;
+
+    @Bean
+    public FilterRegistrationBean catFilter() {
+        initCatLogger();
+
+        ClientConfigProperty property = new ClientConfigProperty();
+        property.setAppId(applicationName);
+        property.setPort(port);
+        property.setHttpPort(httpPort);
+        property.setServers(servers);
+
+        Cat.initialize(property);
+
+        DubboCat.enable();
+
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        CatFilter filter = new CatFilter();
+        registration.setFilter(filter);
+        registration.addUrlPatterns("/*");
+        registration.setName("cat-filter");
+        registration.setOrder(1);
+
+        return registration;
+    }
+
+    private void initCatLogger() {
+        //初始化CAT本地日志；
+        String userHome = System.getProperty("user.home");
+        if (userHome.endsWith("/")) {
+            userHome = userHome.substring(0, userHome.length() - 1);
+        }
+        String catLogHome = String.format("%s/logs/%s/cat", userHome, applicationName);
+        File catLogHomePath = new File(catLogHome);
+        if (!catLogHomePath.exists()) {
+            catLogHomePath.mkdirs();
+        } else if (!catLogHomePath.isDirectory()) {
+            catLogHomePath.deleteOnExit();
+            catLogHomePath.mkdirs();
+        }
+
+        CatLogger.updateLogHome(catLogHome);
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getHttpPort() {
+        return httpPort;
+    }
+
+    public void setHttpPort(int httpPort) {
+        this.httpPort = httpPort;
+    }
+
+    public String[] getServers() {
+        return servers;
+    }
+
+    public void setServers(String[] servers) {
+        this.servers = servers;
+    }
+
+    public String getApplicationName() {
+        return applicationName;
+    }
+
+    public void setApplicationName(String applicationName) {
+        this.applicationName = applicationName;
+    }
+}
--- leqee-boot-autoconfiguration/src/main/java/com/leqee/boot/autoconfiguration/dubbo/LeqeeBootDubboAutoConfiguration.java
+++ leqee-boot-autoconfiguration/src/main/java/com/leqee/boot/autoconfiguration/dubbo/LeqeeBootDubboAutoConfiguration.java
@@ -2,11 +2,13 @@ package com.leqee.boot.autoconfiguration.dubbo;
 
 import org.apache.dubbo.config.ApplicationConfig;
 import org.apache.dubbo.config.RegistryConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,12 +17,8 @@ import java.util.Map;
 @ConditionalOnClass(
         name = {"org.apache.dubbo.config.spring.context.annotation.EnableDubbo"}
 )
-@ConfigurationProperties(prefix = "leqee-boot")
+@PropertySource("classpath:leqee-boot.properties")
 public class LeqeeBootDubboAutoConfiguration {
-
-    private static final String APPLICATION_NAME_PROPERTY = "spring.application.name";
-
-    private static final String APPLICATION_ENV_PROPERTY = "spring.application.env";
 
     private static final Map<String, String> DUBBO_REGISTRY_URLS = new HashMap<>();
 
@@ -33,23 +31,29 @@ public class LeqeeBootDubboAutoConfiguration {
         DUBBO_REGISTRY_URLS.put("production", "zookeeper://10.0.16.134:2181");
     }
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${leqee-boot.env:dev}")
+    private String applicationEnv;
+
     @Bean
     @ConditionalOnMissingBean
+    @ConfigurationProperties(prefix = "dubbo.application")
     public ApplicationConfig applicationConfig() {
         ApplicationConfig applicationConfig = new ApplicationConfig();
-        String name = System.getProperty(APPLICATION_NAME_PROPERTY);
-        applicationConfig.setName(name);
+        applicationConfig.setName(applicationName + "-from-auto-configuration");
 
         return applicationConfig;
     }
 
     @Bean
     @ConditionalOnMissingBean
+    @ConfigurationProperties(prefix = "dubbo.registry")
     public RegistryConfig registryConfig() {
         RegistryConfig registryConfig = new RegistryConfig();
-        String env = System.getProperty(APPLICATION_ENV_PROPERTY);
-        if (DUBBO_REGISTRY_URLS.containsKey(env)) {
-            registryConfig.setAddress(DUBBO_REGISTRY_URLS.get(env));
+        if (DUBBO_REGISTRY_URLS.containsKey(applicationEnv)) {
+            registryConfig.setAddress(DUBBO_REGISTRY_URLS.get(applicationEnv));
         } else {
             registryConfig.setAddress(DEFAULT_ZOOKEEPER_URL);
         }
